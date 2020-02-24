ea6ea76bded9a3d056a5c9cb8b189a23111d6630
Elena Anghel <elenamarianaanghel@gmail.com>
2020-02-24 20:26:41 +0200

    Added Help and Sound classes from A Biginners guide book

---
+++ src/main/java/com/elenaciuca/home/exercises/aBeginnersGuide/Sound.java
@@ -0,0 +1,11 @@
+package com.elenaciuca.home.exercises.aBeginnersGuide;
+/*compute the distance to a lightning strike whose sound takes 7.2 seconds to reach you*/
+
+public class Sound {
+    public static void main(String[] args) {
+    double distance;
+        distance = 7.2 * 1100;
+
+        System.out.println("The lightning strike is " + distance + " feets away");
+    }
+}

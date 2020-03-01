3b6a72bc4ae7e917f0f04e343929d6bd009cd234
Elena Anghel <elenamarianaanghel@gmail.com>
2020-03-01 20:42:52 +0200

    Banking Application

--- src/main/java/com/elenaciuca/home/exercises/banking/BankingApp.java
+++ src/main/java/com/elenaciuca/home/exercises/banking/BankingApp.java
@@ -1,4 +1,18 @@
 package com.elenaciuca.home.exercises.banking;
 
+/**
+ *
+ * O banca nou infiintata doreste o aplicatie prin care angajatii sa poata
+ * vizualiza soldul conturilor, sa poata crea noi conturi si sa poata face transferuri intre
+ * cele doua conturi.
+ *
+ * Datele create de aplicatie trebuie salvate pe disk pentru a putea fi utilizate si in
+ * urma unei reporniri a aplicatiei/sistemului.
+ *
+ * Pentru versiunea
+ *
+ *
+ */
 public class BankingApp {
+
 }

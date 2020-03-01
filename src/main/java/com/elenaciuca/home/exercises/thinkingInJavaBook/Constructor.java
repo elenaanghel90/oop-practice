5ec43392108d82cbef7b98a54fb21e1f58727f50
Elena Anghel <elenamarianaanghel@gmail.com>
2020-03-01 20:41:02 +0200

    Exercising with the constructor/no constructor from Thinking in Java book

--- src/main/java/com/elenaciuca/home/exercises/thinkingInJavaBook/Constructor.java
+++ src/main/java/com/elenaciuca/home/exercises/thinkingInJavaBook/Constructor.java
@@ -1,4 +1,21 @@
 package com.elenaciuca.home.exercises.thinkingInJavaBook;
 
 public class Constructor {
+    String string = "abc";
+    String string2;
+    String string3;
+
+    Constructor(String string2){
+        this.string2 = string2;
+        string3 ="fs";
+    }
+
+
+    public static void main(String[] args) {
+        Constructor constructor = new Constructor("dsaa");
+        System.out.println(constructor.string);
+        System.out.println(constructor.string2);
+        System.out.println(constructor.string3);
+    }
+
 }
--- src/main/java/com/elenaciuca/home/exercises/thinkingInJavaBook/NoConstructor.java
+++ src/main/java/com/elenaciuca/home/exercises/thinkingInJavaBook/NoConstructor.java
@@ -1,4 +1,11 @@
 package com.elenaciuca.home.exercises.thinkingInJavaBook;
 
 public class NoConstructor {
+
+    String string;
+
+    public static void main(String[] args) {
+        NoConstructor noConstructor = new NoConstructor();
+        System.out.println(noConstructor.string);
+    }
 }
